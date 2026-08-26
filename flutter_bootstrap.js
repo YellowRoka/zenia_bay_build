0f6f1cd6deecc9b521c1e634734aab1db28dc49d
fox <fonadgergo@hotmail.com>
2026-08-26 23:02:13 +0200

    2026_08_26_22:26

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4036201964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "318000878" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
