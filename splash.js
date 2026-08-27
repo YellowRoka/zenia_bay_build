79b1f75b4f10d4c197b3228cf31d86e99a62ec4b
fox <fonadgergo@hotmail.com>
2026-08-27 23:17:49 +0200

    2026_08_27_23:17

---
+++ splash.js
@@ -0,0 +1,20 @@
+var splashFadeDelayMs = 500;
+var splashFadeDurationMs = 900;
+
+window.addEventListener("flutter-first-frame", function () {
+  var splash = document.getElementById("launch-splash");
+  if (!splash) {
+    return;
+  }
+
+  window.requestAnimationFrame(function () {
+    window.requestAnimationFrame(function () {
+      window.setTimeout(function () {
+        splash.classList.add("is-hidden");
+        window.setTimeout(function () {
+          splash.remove();
+        }, splashFadeDurationMs + 40);
+      }, splashFadeDelayMs);
+    });
+  });
+});
